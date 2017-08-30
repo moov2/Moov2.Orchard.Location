042b231641e5e098ec5f796f76a177e7a0902a82
Lawrence Dine <lawrence.dine@moov2.com>
2017-08-30 11:44:38 +0100

    Update location edit fields to reduce clutter
    Update the location part editing fields to reduce the clutter by making
    them hidden when they have no content. This means when editing the user
    won't be overwhelmed with potentially unnecessary location fields.
    
    Add a 'show all' link that will un-hide all the hidden location fields.
    
    Add a separate link that will show the coordinate fields.

---
+++ Assets/JavaScript/orchard.location.js
@@ -0,0 +1,33 @@
+﻿(function () {
+
+    var $locationFields = $('.js-location-field'),
+        $coordFields = $('.js-location-field-coords'),
+        $showAllLink = $('.js-show-location-all'),
+        $showCoordsLink = $('.js-show-location-coordinates'),
+        HIDDEN_CLASS = 'location-admin--hidden';
+
+    var showLocations = function () {
+        $locationFields.removeClass(HIDDEN_CLASS);
+    };
+
+    var showCoords = function () {
+        $coordFields.removeClass(HIDDEN_CLASS);
+    };
+
+    var addListeners = function () {
+        $showAllLink.on('click', function (e) {
+            e.preventDefault();
+            $showAllLink.addClass(HIDDEN_CLASS);
+            showLocations();
+        });
+
+        $showCoordsLink.on('click', function (e) {
+            e.preventDefault();
+            $showCoordsLink.addClass(HIDDEN_CLASS);
+            showCoords();
+        });
+    };
+
+    addListeners();
+
+})();
---
+++ ResourceManifest.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.UI.Resources;
+
+namespace Moov2.Orchard.Location
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            builder.Add()
+                .DefineScript("LocationJs").SetUrl("orchard.location.min.js", "orchard.location.js");
+            builder.Add()
+                .DefineStyle("LocationAdminStyle").SetUrl("admin-location.css");
+        }
+    }
+}
